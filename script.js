27f2f05455cfe96e4fe9ff5168894a48f3e4fad5
Vishal Kumar <136982736+humptinessvishal@users.noreply.github.com>
2023-09-30 14:53:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 // Your script here.
+const synth = window.speechSynthesis;
+const voicesDropdown = document.querySelector('#voice');
+const voices = synth.getVoices();
+voicesDropdown.innerHTML = voices
+	.filter(voice => voice.lang.includes('en'))
+	.map(voice => `<option value="${voice.name}">${voice.name}</option>`)
+	.join('');
+
+document.querySelector('#start').addEventListener('click', () => {
+	const text = document.querySelector('#text').value;
+    const voice = voices.find(voice => voice.name === voicesDropdown.value);
+	const utterance = new SpeechSynthesisUtterance(text);
+	utterance.voice = voice;
+	synth.speak(utterance);
+});
+
+    // Stop speaking when the stop button is clicked
+    document.querySelector('#stop').addEventListener('click', () => {
+      synth.cancel();
+    });
